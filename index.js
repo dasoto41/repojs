03a8c611b9bcc4078e6bbd66416b91072ef7d9b3
daniela soto <danysoto41@gmail.com>
2022-06-29 17:08:27 -0300

    primer version

---
+++ index.js
@@ -0,0 +1,12 @@
+let entrada = prompt("Ingresar un numero");
+if (entrada <= 5){
+    alert("el numero es menor o igual a 5")
+}
+else if(entrada >5 && entrada <10){
+    alert("el numero es mayor a 5 y menor que 10")
+
+}
+
+else{
+    alert("el numero es mayor o igual a 10")
+}
